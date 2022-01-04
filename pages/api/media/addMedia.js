c346ce4fc6344e773b32fa34bf635a499455f986
Franckapik <fanch44@hotmail.com>
2022-01-04 09:45:27 +0100

    autoincrement issue on media

--- pages/api/media/addMedia.js
+++ pages/api/media/addMedia.js
@@ -6,6 +6,14 @@ const prisma = new PrismaClient();
 export default async (req, res) => {
   const data = req.body;
 
+  //issue on auto-increment
+  const medias = await prisma.media.findMany();
+  const lastId = medias.sort((a, b) => a.media_id - b.media_id)[
+    medias.length - 1
+  ].media_id;
+
+  data.media_id = data.media_id || lastId + 1;
+
   try {
     const result = await prisma.media.upsert({
       where: {
@@ -20,6 +28,8 @@ export default async (req, res) => {
     });
     res.status(200).json(result);
   } catch (err) {
+    console.log(err);
+
     res.status(403).json({ err: "Error occured while adding a new media." });
   }
 };
