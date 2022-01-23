5095f0c5a4cc0b9207dfc638de3e01ad6a0125ec
Franckapik <fanch44@hotmail.com>
2022-01-23 14:29:59 +0100

    on the road to tree folders

--- pages/admin/explorer/index.js
+++ pages/admin/explorer/index.js
@@ -6,6 +6,30 @@ import axios from "axios";
 import { useRouter } from "next/dist/client/router";
 import { getSession } from "next-auth/client";
 import { traverse } from "fs-tree-utils";
+import "react-folder-tree/dist/style.css";
+import dynamic from "next/dynamic";
+
+const DynamicFileTreeImport = dynamic(() => import("react-folder-tree"), {
+  ssr: false, // needed to prevent warning about dynamic component
+});
+
+const treeState = {
+  name: "root [half checked and opened]",
+  checked: 0.5, // half check: some children are checked
+  isOpen: true, // this folder is opened, we can see it's children
+  children: [
+    { name: "children 1 [not checked]", checked: 0 },
+    {
+      name: "children 2 [half checked and not opened]",
+      checked: 0.5,
+      isOpen: false,
+      children: [
+        { name: "children 2-1 [not checked]", checked: 0 },
+        { name: "children 2-2 [checked]", checked: 1 },
+      ],
+    },
+  ],
+};
 
 const { PrismaClient } = require("@prisma/client");
 
@@ -20,38 +44,92 @@ const MediaPage = ({ db_media, files }) => {
     setShow(!show);
     router.push("/admin/media");
   };
-  var tableauFormaté = files.map((a) => {
+  var arr2 = files.map((a) => {
     var b = {};
     b["name"] = a.item;
     b["type"] = a.path.includes(".") ? "file" : "folder";
     b["size"] = a.stats.size;
     b["createdAt"] = a.stats.atime;
+    b["parent"] = a.container;
+    b["path"] = a.path;
     return b;
   });
-  console.log(tableauFormaté);
-  var containers = files.map((a, i) => a.container);
-  const uniq = [...new Set(containers)];
+
+  const onTreeStateChange = (state, event) => console.log(state, event);
+
+  var arr3 = arr2.map((a, i) => a.path);
+
+  function buildTree(pathes, getValueCB) {
+    var currentPath,
+      lastPath,
+      node,
+      parent,
+      map = {
+        "": {
+          children: [],
+        },
+      },
+      stack = [""];
+
+    for (let path of pathes) {
+      let nodes = path.split("/");
+      for (let i = 0; i < nodes.length; i++) {
+        currentPath = "/" + nodes.slice(1, i + 1).join("/");
+        lastPath = stack[stack.length - 1];
+        parent = map[lastPath];
+        if (!map[currentPath]) {
+          node = {
+            name: currentPath,
+            value: getValueCB(currentPath),
+            children: [],
+          };
+          parent.children.push(node);
+          map[currentPath] = node;
+        }
+        stack.push(currentPath);
+      }
+      stack = stack.slice(0, 1);
+    }
+    return map[""].children[0];
+  }
+
+  function getFileSizeSync() {
+    return 200;
+  }
+  var tree = buildTree(arr3, function (path) {
+    return getFileSizeSync(path);
+  });
+
+  console.log(tree);
 
   return (
     <Layout_Admin title={"Medias"}>
       <Row>
-        <Table striped borderless hover className="mt-5">
-          <thead>
-            <tr>
-              <th>Artistes</th>
-            </tr>
-          </thead>
-          <tbody>
-            {uniq &&
-              uniq.map((a, i) => {
-                return (
-                  <tr key={"jenesaispas"}>
-                    <td key={"a" + i}>{a}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </Table>
+        <DynamicFileTreeImport
+          data={tree}
+          onChange={onTreeStateChange}
+          showCheckbox={false}
+          indentPixels={100}
+        />
+        {
+          <Table striped borderless hover className="mt-5">
+            <thead>
+              <tr>
+                <th>Artistes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tree &&
+                Object.keys(tree).map((a, i) => {
+                  return (
+                    <tr key={"jenesaispas"}>
+                      <td key={"a" + i}>{a}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        }
       </Row>
 
       <Modal show={show} onHide={() => setShow(false)}>
