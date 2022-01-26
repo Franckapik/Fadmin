f7946037e39800d94badb1d71cc831a7486aaebe
Franckapik <fanch44@hotmail.com>
2022-01-26 08:16:44 +0100

    simplest mediahome

--- components/mediashome.js
+++ components/mediashome.js
@@ -3,19 +3,10 @@ import { Card, Container, Figure } from "react-bootstrap";
 
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import Link from "next/link";
 
 export const MediasHome = ({ mediasFiles, setShow, show }) => {
   const router = useRouter();
-  const [pos, setPos] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPos((pos) => ++pos);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [pos]);
-
-  console.info(mediasFiles.length);
 
   return (
     <Container
@@ -27,29 +18,28 @@ export const MediasHome = ({ mediasFiles, setShow, show }) => {
       {mediasFiles && mediasFiles != 0
         ? mediasFiles.map((a, i) => {
             return (
-              <Card
-                border="0"
-                className="cursor m-3 filtre barre"
-                key={i}
-                style={{
-                  top: `${i * Math.random()}vh`,
-                  height: `${Math.floor(1 * (40 - 25 + 1) + 25)}vh`,
-                }}
-              >
-                <Card.Body
-                  className="fade-short "
+              <Link href={`/authors/${a.author.author_id}`} key={i} passHref>
+                <Card
+                  border="0"
+                  className="cursor m-3 filtre-hard barre"
                   style={{
-                    width: "70px",
-                    backgroundImage: `url("${
-                      mediasFiles[(i + pos) % mediasFiles.length].media_path
-                    }")`, ///medias/1/03/03.png
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center center",
-                    backgroundSize: "cover",
-                    borderRadius: "50px 50px 3%",
+                    top: `${i * Math.random() * 2}vh`,
+                    height: `${Math.floor(1 * (40 - 25 + 1) + 25)}vh`,
                   }}
-                ></Card.Body>
-              </Card>
+                >
+                  <Card.Body
+                    className="fade-short "
+                    style={{
+                      width: "70px",
+                      backgroundImage: `url("${a.media_path}")`, ///medias/1/03/03.png
+                      backgroundRepeat: "no-repeat",
+                      backgroundPosition: "center center",
+                      backgroundSize: "cover",
+                      borderRadius: "50px 50px 3%",
+                    }}
+                  ></Card.Body>
+                </Card>
+              </Link>
             );
           })
         : null}{" "}
