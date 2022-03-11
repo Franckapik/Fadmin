1b02d0707f2dd7c5d4c79b64fb1637a1daf6875e
Franckapik <fanch44@hotmail.com>
2022-03-11 10:18:42 +0100

    socials on contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,9 +3,9 @@ import { ContactForm } from "../../components/contactform";
 import Layout_Home from "../../layouts/layout_home";
 import prisma from "../../prisma/prisma";
 
-export default function Comment({ db_authors }) {
+export default function Comment({ db_authors, db_home }) {
   return (
-    <Layout_Home authors={db_authors}>
+    <Layout_Home authors={db_authors} home={db_home}>
       <ContactForm db_authors={db_authors}></ContactForm>
     </Layout_Home>
   );
@@ -18,7 +18,13 @@ export async function getServerSideProps({ params, query }) {
     },
   });
 
+  const db_home = await prisma.home.findUnique({
+    where: {
+      home_id: 1,
+    },
+  });
+
   return {
-    props: { db_authors },
+    props: { db_authors, db_home },
   };
 }
