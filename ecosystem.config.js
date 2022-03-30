5cff9fe8ddfa7854f59f131564378b3f30fc80e8
franckapik <fanch44@hotmail.com>
2022-03-30 17:48:46 +0200

    no watch on pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,6 @@ module.exports = {
       name: "qualyn",
       script: "npm",
       args: "start",
-      watch: true,
-      watch_delay: 15000,
-    },
-    {
-      name: "qualyn-build",
-      script: "npm",
-      args: "run build",
-      watch: ["./public"],
-      autorestart: false,
     },
   ],
 };
